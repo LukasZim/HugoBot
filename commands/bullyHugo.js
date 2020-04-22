ddb76f79c109d712cd5e86043ec2084cab882586
Lukas Zimmerhackl <lukas.zim@hotmail.com>
2020-04-22 19:11:40 +0200

    made bullyHugo more logical

--- commands/bullyHugo.js
+++ commands/bullyHugo.js
@@ -6,7 +6,7 @@ module.exports.run = async (bot, message, args) => {
     var target = bot.guilds.cache.array()[0].members.cache.array().find(function (element) {
         return element.id == 321034764268077066;
     });
-    target.setNickname(message.content.replace("bullyHugo ", ''));
+    target.setNickname(args);
     return message.channel.send("Hugo's new nickname is: " + message.content.replace("bullyHugo ", ''));
 }
 
