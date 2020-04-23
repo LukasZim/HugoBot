30fa8f286f9a30bbc4dc07dbf69a788cb51b87f0
Lukas Zimmerhackl <lukas.zim@hotmail.com>
2020-04-23 23:39:32 +0200

    added skip functionality

--- commands/hugoSkip.js
+++ commands/hugoSkip.js
@@ -1,7 +1,14 @@
 const discord = require("discord.js");
 
 module.exports.run = async(bot, message, args, options) => {
-    console.log("halleu");
+    if(!options || !options.active || !options.active.get(message.guild.id) || !options.active.get(message.guild.id).queue) return message.channel.send("Queue is empty!");
+
+
+    var data = options.active.get(message.guild.id);
+    var queue = data.queue;
+    if(!data.dispatcher) return message.channel.send("No song playing");
+    //if(!(message.member.voice.channel.id == bot.guilds.cache.get(dispatcher.guildID).me.voice.channel.id)) return message.channel.send("Not in my channel");
+    data.dispatcher.end();
 }
 
 module.exports.help = {
--- commands/showQueue.js
+++ commands/showQueue.js
@@ -3,12 +3,12 @@ const discord = require("discord.js");
 module.exports.run = async(bot, message, args, options) => {
     var textMessage = "";
     if(!options || !options.active || !options.active.get(message.guild.id) || !options.active.get(message.guild.id).queue) return message.channel.send("Queue is empty!");
-    console.log(options.active.get(message.guild.id).queue.length);
-    console.log(typeof options.active.get(message.guild.id).queue)
+    //console.log(options.active.get(message.guild.id).queue.length);
+    //console.log(typeof options.active.get(message.guild.id).queue)
     //console.log(textMessage);
-    console.log(options.active.get(message.guild.id).queue);
+    //console.log(options.active.get(message.guild.id).queue);
     for(i = 0; i < options.active.get(message.guild.id).queue.length; i++){
-        textMessage = textMessage + options.active.get(message.guild.id).queue[i].songTitle + "\n";
+        textMessage = textMessage + i + " : " + options.active.get(message.guild.id).queue[i].songTitle + "\n";
     }
     message.channel.send(textMessage);
 }
