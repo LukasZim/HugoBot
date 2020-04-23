8f86a70d6effe0789988c2035370d9c0b74212d3
Lukas Zimmerhackl <lukas.zim@hotmail.com>
2020-04-23 22:37:50 +0200

    Update showQueue.js

--- commands/showQueue.js
+++ commands/showQueue.js
@@ -1,7 +1,7 @@
 const discord = require("discord.js");
 
 module.exports.run = async(bot, message, args, options) => {
-    console.log(options.active);
+    console.log(options.active.get("queue"));
 }
 
 module.exports.help = {
