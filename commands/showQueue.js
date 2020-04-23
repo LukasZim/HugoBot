6974655aced1360b668eee3747380f9192e90b12
Lukas Zimmerhackl <lukas.zim@hotmail.com>
2020-04-23 22:00:44 +0200

    Create showQueue.js

---
+++ commands/showQueue.js
@@ -0,0 +1,9 @@
+const discord = require("discord.js");
+
+module.exports.run = async(bot, message, args, options) => {
+    console.log(options);
+}
+
+module.exports.help = {
+    name: "showQueue"
+}
