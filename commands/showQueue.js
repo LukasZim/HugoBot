5dd183f3a830a67ed68cd2d01002838b46dcc031
Lukas Zimmerhackl <lukas.zim@hotmail.com>
2020-04-23 23:01:09 +0200

    Update showQueue.js

--- commands/showQueue.js
+++ commands/showQueue.js
@@ -2,7 +2,7 @@ const discord = require("discord.js");
 
 module.exports.run = async(bot, message, args, options) => {
     var textMessage = "";
-    options.active.get(message.guild.id).queue.forEach(element => console.log(element));
+    options.active.get(message.guild.id).queue.forEach(element => console.log(element.songTitle));
     console.log(textMessage);
     console.log(options.active.get(message.guild.id).queue);
     message.channel.send(textMessage);
