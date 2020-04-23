41617c0d7679cf646276ec3b8ae72ad316764400
Lukas Zimmerhackl <lukas.zim@hotmail.com>
2020-04-23 23:17:24 +0200

    Update showQueue.js

--- commands/showQueue.js
+++ commands/showQueue.js
@@ -4,6 +4,7 @@ module.exports.run = async(bot, message, args, options) => {
     var textMessage = "";
     if(!options || !options.active || !options.active.get(message.guild.id) || !options.active.get(message.guild.id).queue) return message.channel.send("Queue is empty!");
     console.log(options.active.get(message.guild.id).queue.length);
+    console.log(typeof options.active.get(message.guild.id).queue)
     //console.log(textMessage);
     console.log(options.active.get(message.guild.id).queue);
     //message.channel.send(textMessage);
