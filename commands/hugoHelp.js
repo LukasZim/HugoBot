8e925f3f7274c32d8069998ddb6c53bb0fa9da6c
LukasZim <lukas.zim00@gmail.com>
2021-04-19 18:25:28 +0200

    Update hugoHelp.js

--- commands/hugoHelp.js
+++ commands/hugoHelp.js
@@ -3,7 +3,11 @@ const discord = require("discord.js");
 module.exports.run = async(bot, message, args) => {
     let arr = Array.from(bot.commands.keys());
     console.log(bot.commands.values());
-    message.channel.send(arr.join("\n"))
+    let str = "";
+    for (cur in Array.from(bot.commands.values())) {
+        str += cur.help.name + ": " + cur.help.description;
+    }
+    message.channel.send(str)
 }
 
 module.exports.help = {
