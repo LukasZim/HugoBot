58f0262ae578ec6987239899b3c77f73acb5a0e5
LukasZim <lukas.zim00@gmail.com>
2021-04-19 19:45:14 +0200

    Update hugoHelp.js

--- commands/hugoHelp.js
+++ commands/hugoHelp.js
@@ -4,14 +4,13 @@ module.exports.run = async(bot, message, args) => {
     let arr = Array.from(bot.commands.values());
     console.log(bot.commands.values());
     let str = "";
-    let fields = []
 
     const embed = new discord.MessageEmbed()
-        .setColor("#0099ff")
+        // .setColor("#0099ff")
         .setTitle("All commands I can pull from my Nipples!!")
-        .setURL("https://pornhub.com/gayporn")
-        .setAuthor("Hugo de Heer bot", "https://www.facebook.com/hugo.deheer.7")
-        .addThumbnail('https://pbs.twimg.com/profile_images/464788131967807488/1tEzXaF8.jpeg')
+        // .setURL("https://pornhub.com/gayporn")
+        // .setAuthor("Hugo de Heer bot", "https://www.facebook.com/hugo.deheer.7")
+        // .addThumbnail('https://pbs.twimg.com/profile_images/464788131967807488/1tEzXaF8.jpeg')
         
     for (let i = 0; i < arr.length; i++) {
         embed.addField(arr[i].help.name, arr[i].help.description, false)
