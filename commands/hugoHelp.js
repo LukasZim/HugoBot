951f47bca09eda9aecb4825af25d42cb5f111dcd
LukasZim <lukas.zim00@gmail.com>
2021-04-19 17:54:19 +0200

    added necessary replys

--- commands/hugoHelp.js
+++ commands/hugoHelp.js
@@ -1,8 +1,7 @@
 const discord = require("discord.js");
 
 module.exports.run = async(bot, message, args) => {
-    bot.commands.foreach(x => console.log(x));
-    console.log("hugo is helpende");
+    console.log(bot.commands)
 }
 
 module.exports.help = {
