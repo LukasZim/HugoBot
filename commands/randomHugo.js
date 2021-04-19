d4f25a1c5a8f550d7d4ee1c40336c49b4c8aa7c9
LukasZim <lukas.zim00@gmail.com>
2021-04-19 20:30:48 +0200

    Update randomHugo.js

--- commands/randomHugo.js
+++ commands/randomHugo.js
@@ -10,7 +10,8 @@ module.exports.run = async(bot, message, args) => {
     // console.log("1 " + await hug(321034764268077066));
     // console.log("2 " +await message.guild.members.fetch(321034764268077066));
     console.log("wut");
-    console.log("3 " + message.guild.members.cache.get(321034764268077066));
+    console.log("3 " + message.guild.members.cache.get("321034764268077066"));
+    console.log("mem" + message.guild.members.get("321034764268077066"));
     const hugo = await hug(321034764268077066);
     console.log(hugo);
     var newNick = shuffle(hugo.nickname);
