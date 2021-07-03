37d918c0ffac5e8c09cbb5902c768f27e9831d0e
LukasZim <lukas.zim00@gmail.com>
2021-07-03 15:42:05 +0200

    Update index.js

--- index.js
+++ index.js
@@ -233,9 +233,9 @@ function davidGreenText(hugosMessage){
                 > know they would want privacy, sit down at my computer
                 > open discord
                 > think of a convoluted way in which I can relate homosexuality and falsehood to the current situation
-                (> suck the cheeto dust off my fingers as I begin to type my masterpiece in the comment section)
+                > (suck the cheeto dust off my fingers as I begin to type my masterpiece in the comment section)
                 > ${hugosMessage.replace(/\n+$/, "").replace(/\n/g, "\n>") + ""}
-                (> giggle as I imagine the intellectuals in discord chat perusing my incredibly witty and original comment)
+                > (giggle as I imagine the intellectuals in discord chat perusing my incredibly witty and original comment)
                 > hear my mom moan with ecstasy as Chad floods her fertile womb with his seed
                 > it's been a good day
                 > i'll get lots of attention for my impressive contribution to internet culture, and Chad might even let me eat his cum out of my wife's pussy if he finds my comment funny enough
